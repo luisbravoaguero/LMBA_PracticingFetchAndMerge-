aa4723c0bdbd8b1ad35be3fc5708f94a424259bb
luisbravoaguero <luisbravoaguero@gmail.com>
2022-09-21 09:46:49 -0500

    comentario anadido desde github

--- src/main/java/Base/MainClass.java
+++ src/main/java/Base/MainClass.java
@@ -3,6 +3,7 @@ package Base;
 public class MainClass {
 
     public static void main(String[] args){
+        //this is a comment line created from github to test fetch and merge in local directory
         System.out.println("This is my first line");
     }
 }
